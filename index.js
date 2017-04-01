e43378dc7fefd57126bec891f4a3a15ad27bc481
vicky.antre <vicky.antre@gmail.com>
2017-04-01 08:32:30 +0300

    ended homework

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 var http = require('http');
 var port = 3000;
+var calc = require('calculator');
 
 var server = http.createServer(function(request, response) {
     switch (request.url) {
         case '/':
             response.end("Hello");
-            break;
+            return;
+        case '/favicon.ico':
+          response.end();
+          return;
     }
+    var f = calc.func('f(x) = ' + request.url.slice(1));
+    response.end(f().toString());
 });
 
 server.listen(port, function() {
